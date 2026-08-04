d85abbb25378676fac1ae20171e209ff1d0c80aa
imjohnclinton <johnclintonkora@gmail.com>
2026-08-04 21:57:29 +0000

    add some exploratory data

--- src/03_exploratory_data_analysis.py
+++ src/03_exploratory_data_analysis.py
@@ -1,8 +1,48 @@
 # Using Streamlit or Dash
-import streamlit as st
-import plotly.express as px
-import plotly.graph_objects as go
- 
+import pandas as pd
+
+utilities=pd.read_csv("data/utilities.csv")
+substations=pd.read_csv("data/substations.csv")
+lines=pd.read_csv("data/lines.csv")  
+
+print(substations[['Latitude','Longitude','Voltage (kV)','Capacity (MVA)','Commissioning Year']].describe())
+print("-----------------------------------------------------------------------------------")
+print(lines[['Voltage (kV)','Length (km)','Capacity (MVA)']].describe())
+print("-----------------------------------------------------------------------------------")
+
+print('lines')
+print(lines['Destination Substation'].value_counts())
+print("-----------------------------------------------------------------------------------")
+print(lines['Source Substation'].value_counts())
+print("-----------------------------------------------------------------------------------")
+print(lines['Status'].value_counts())
+print("-----------------------------------------------------------------------------------")
+print(lines['Line Type'].value_counts())
+print("-----------------------------------------------------------------------------------")
+
+
+
+print('Substations')
+print(substations['Region'].value_counts())
+print("-----------------------------------------------------------------------------------")
+print(substations['Country'].value_counts())
+print("-----------------------------------------------------------------------------------")
+print(substations['Type'].value_counts())
+print("-----------------------------------------------------------------------------------")
+print(substations['Status'].value_counts())
+print("-----------------------------------------------------------------------------------")
+
+print(f'Utilities')
+print(utilities['Country'].value_counts())
+print("-----------------------------------------------------------------------------------")
+print(utilities['Type'].value_counts())
+print("-----------------------------------------------------------------------------------")
+print(utilities['Active'].value_counts())
+print("-----------------------------------------------------------------------------------")
+
+print("Top Utilities")
+topUtilities = lines['Utility ID'].value_counts().to_dict()
+print(topUtilities)
 # Dashboard components:
 # - Executive summary with key metrics
 # - Interactive map with filtering options (region, voltage, utility)
